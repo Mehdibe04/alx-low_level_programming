2915c6e351c3575d5b74bfab3f7f36e38fbd53c3
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-14 14:20:52 -0700

    6cm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all digit numbers of base 10 using putchar
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0, i < 10, i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
