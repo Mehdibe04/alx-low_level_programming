215321dd4d4f9377ea0d333f01b2ca27a9eab033
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-14 15:18:13 -0700

    9th cm updt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,14 @@ int main(void)
 {
 	int i = 48;
 
-	while (i < 57)
+	while (i <= 57)
 	{
 		putchar(i);
-		putchar(44);
-		putchar(32);
+		if (i != 57)
+			putchar(44);
+			putchar(32);
 		i++;
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }
