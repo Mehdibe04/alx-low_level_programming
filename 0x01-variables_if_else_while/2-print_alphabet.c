317a5ffbcdf70b9905846b70df3f202175536c39
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-14 13:36:52 -0700

    commit n2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a function that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 97
+
+	while (i <= 122)
+		putchar(i);
+		i++;
+	return (0);
+}
