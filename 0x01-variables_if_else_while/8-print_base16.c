508249864d2807240758a8c44c170ed772475e5e
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-14 14:56:12 -0700

    8th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all digit numbers of base 10 using putchar
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 0; i < 6; i++)
+		putchar(i + 'A');
+	putchar('\n');
+	return (0);
+}
