5ee76a471ae70c7b8c65940d856669f7ca6d9ce5
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-14 19:40:46 -0700

    102th updated

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,17 @@
 
 int main(void)
 {
-	int dig1 = 0;
-	int dig2;
+	int dig1 = 0, int dig2;
 
 	while (dig1 <= 99)
 	{
 		dig2 = dig1;
 		while (dig2 <= 99)
 		{
-			if (dig1 != dig2)
+			if (dig2 != dig1)
 			{
 				putchar((dig1 / 10) + 48);
-				putchar((dig2 % 10) + 48);
+				putchar((dig1 % 10) + 48);
 				putchar(' ');
 				putchar((dig2 / 10) + 48);
 				putchar((dig2 % 10) + 48);
