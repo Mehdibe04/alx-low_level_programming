f47e385b48bd970f9e4bdedaf64f3f10bf5a5b8a
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-18 11:41:29 -0700

    10th cm

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: res of a + b
+*/
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
