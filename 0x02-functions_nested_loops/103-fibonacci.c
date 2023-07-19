2804a5a22ffcadec423a5cb62e084089421b4c23
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-18 18:30:47 -0700

    103th cm

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the sum of even-valued terms of
+ *              Fibonacci sequence under 4000000
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	unsigned long fibO = 0, fibC = 1, fibN, sum = 0;
+	int i;
+
+	while (fibN < 4000000)
+		fibN = fibO + fibC;
+		if (fibN % 2 == 0)
+			sum += fibN;
+		fibO = fibC;
+		fibC = fibN;
+	print("%ld\n", sum);
+	return (0);
+}
