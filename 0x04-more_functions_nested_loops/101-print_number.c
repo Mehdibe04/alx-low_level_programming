fd9b95d6682b320dfc824f0008c99aa474cb1bdd
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-20 17:23:58 -0700

    101th com updt

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,62 +2,23 @@
 #include <math.h>
 
 /**
- * size_num - returns thenumber of digits of a number
+ * print_number - prints an integer
  *
- * @n: input number
- *
- * Return: size
-*/
-
-int size_num(int n)
-{
-	int size = 0;
-
-	if (n < 0)
-		n = n * (-1);
-
-	while (n / 10 != 0)
-	{
-		size++;
-		n = n / 10;
-	}
-
-	if (n < 10 && n >= 0)
-		size++;
-
-	return (size);
-}
-
-/**
- * print_number - prints a number
- *
- * @n: input number
+ * @n: The integer to be printed
 */
 
 void print_number(int n)
 {
-	int i, m = 0;
-	int s = size_num(n);
-	int size_c = s;
+	unsigned int num = n;
 
-	for (i = 0; i < s; i++)
+	if (n < 0)
 	{
-
-		if (n < 0)
-		{
-			n = (-1) * n;
-			_putchar(45);
-		}
-
-		if (size_c == 1)
-		{
-			_putchar(n + 48);
-			break;
-		}
-
-		m = n / (int)pow(10, size_c - 1);
-		_putchar(m + 48);
-		n = n % (int)pow(10, size_c - 1);
-		size_c--;
+		_putchar('-');
+		num = -num;
 	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + 48);
 }
---
+++ 0x04-more_functions_nested_loops/_print_number.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * size_num - returns thenumber of digits of a number
+ *
+ * @n: input number
+ *
+ * Return: size
+*/
+
+int size_num(int n)
+{
+	int size = 0;
+
+	if (n < 0)
+		n = n * (-1);
+
+	while (n / 10 != 0)
+	{
+		size++;
+		n = n / 10;
+	}
+
+	if (n < 10 && n >= 0)
+		size++;
+
+	return (size);
+}
+
+/**
+ * print_number - prints a number
+ *
+ * @n: input number
+*/
+
+void print_number(int n)
+{
+	int i, m = 0;
+	int s = size_num(n);
+	int size_c = s;
+
+	for (i = 0; i < s; i++)
+	{
+
+		if (n < 0)
+		{
+			n = (-1) * n;
+			_putchar(45);
+		}
+
+		if (size_c == 1)
+		{
+			_putchar(n + 48);
+			break;
+		}
+
+		m = n / (int)pow(10, size_c - 1);
+		_putchar(m + 48);
+		n = n % (int)pow(10, size_c - 1);
+		size_c--;
+	}
+}
