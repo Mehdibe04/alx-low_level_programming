765ee28e778d4009396f9c62d8f738a936b05a3f
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-24 14:51:00 -0700

    6 updt

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,15 @@
 void puts2(char *str)
 {
 	if (*str != '\0' && *(str + 1) == '\0')
-	{
 		_putchar(*str);
 	else
-		str += 2
+	{
+		str += 2;
 		while (*str && *(str + 1))
 		{
 			_putchar(*str);
 			str += 2;
+		}
 	}
 	_putchar('\n');
 }
