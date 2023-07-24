4c2585d0a49100e1ea9d37149ccb787ae5edbd3f
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-24 13:35:56 -0700

    6h cm

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string of characters input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int count = 0;
+	int i, tmp;
+
+	while (s[count])
+	{
+		count++;
+	}
+	for (i = 0; i < count / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[count - i - 1];
+		s[count - i - 1];
+	}
+}
