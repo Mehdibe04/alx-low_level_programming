e029df30eb000ffee0c417e15ca7b97ffea50d1c
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-24 14:03:44 -0700

    7th cm

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ *         starting with the first character
+ *
+ * @str: string of character input
+ *
+ * Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	_putchar(*s);
+	while (*++s)
+	{
+		if (++s != '\0')
+			_putchar(*s);
+	}
+}
