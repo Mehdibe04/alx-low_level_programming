2c84b781de2a56c5061399756666a56dbe6b2854
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-27 15:15:35 -0700

    2nd commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	while (i < n)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
-		else
-			dest[i] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
