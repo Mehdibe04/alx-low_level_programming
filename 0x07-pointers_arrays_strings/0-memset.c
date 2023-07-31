546a6124a52a1334ca3a5de3fd8be771f6078d6b
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-07-31 15:14:20 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: string input
+ * @b: character to be placer a specific number of times
+ * @n: the number of times the char b will be typed
+ *
+ * Return: a pointer to the string
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && i < n)
+	{
+		s[i] = 'b';
+		i++;
+	}
+	return (s);
+}
