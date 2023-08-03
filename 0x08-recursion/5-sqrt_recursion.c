82773d87bfa1821a791e2595edc5325ad00ebca5
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-02 19:09:28 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,33 @@
 #include "main.h"
 
-/***/
+/**
+ * _sqrt_recursion - finds natural square root
+ *
+ * @n: number to be checked
+ *
+ * Return: square root of n
+*/
 
 int _sqrt_recursion(int n)
 {
+	return (square(n, 1));
+}
 
+/**
+ * square - finds square root
+ *
+ * @n: number to be checked
+ * @i: square root
+ *
+ * Return: square root of n
+*/
 
+int square(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i < n)
+		return (square(n, i + 1));
+	else
+		return (-1);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if n is prime
- *
- * @n: number to be checked
- *
- * Return: 1 if prime, 0 otherwise.
-*/
-
-int check_if_divisible(int n, int i);
-int is_prime_number(int n)
-{
-	if (n < 2)
-		return (0);
-	else if (n == 2)
-		return (1);
-	else if (n % 2 == 0)
-		return (0);
-	return (check_if_divisible(n, 3));
-}
-
-/**
  * check_if_divisible - checks if n is prime
  *
  * @n: the number to be checked
@@ -36,4 +16,22 @@ int check_if_divisible(int n, int i)
 	if (n % i == 0)
 		return (0);
 	return (check_if_divisible(n, i + 2));
+}
+
+/**
+ * is_prime_number - checks if n is prime
+ *
+ * @n: number to be checked
+ * Return: 1 if prime, 0 otherwise
+*/
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else if (n % 2 == 0)
+		return (0);
+	return (check_if_divisible(n, 3));
 }
