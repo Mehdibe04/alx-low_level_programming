ed010629269d5f150e3fbf2deb1366374634eb2d
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-04 16:35:17 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ *
+ * @argc: int input
+ * @argv: list input
+ *
+ * Return: 0 (Success)
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return(0);
+}
