1a52256688f51554ce91331c3d953ff65c187848
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-04 18:20:33 -0700

    4th task updt

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,6 @@ int main(int argc, char *argv[])
 	{
 		res = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", res);
-		return (0);
 	}
 
 	else
@@ -25,4 +24,5 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
