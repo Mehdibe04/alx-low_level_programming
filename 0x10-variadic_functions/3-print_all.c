9b5131ce159433abe7868527d29bb7470aa3e156
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-17 14:49:31 -0700

    task 4

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ *
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: Nothing
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s;
+	char *sp = "";
+
+	va_list args;
+
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sp, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sp, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sp, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", sp, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sp = ", ";
+			i++;
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct token - struct token
+ *
+ * @token: format token
+ * @f: the function associated
+*/
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
