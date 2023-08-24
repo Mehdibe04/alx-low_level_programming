3ce6d4a0a15109e2ee10d5f5ae0e1184691b6bb7
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-24 12:36:25 -0700

    updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", _strlen(h->str), h->str);
 		h = h->next;
 		l++;
 	}
