0b7b1b6b5dbfad4250be53341b6909ef1848bead
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-29 13:49:53 -0700

    task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_ndeint_end - adds a new node at the end of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  *
  * @head: head of the list
  * @n: value of the new node
