e54e1f3853bb2a1ee03569d4489e07a8af8b4f59
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-29 15:44:56 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	curr = head;
-	for (; i < n; i++)
+	for (; i < index; i++)
 	{
 		if (curr->next)
 			curr = curr->next;
