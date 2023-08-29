eb96cd304538f5aeecbd9b6954635fb4c701cd38
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-08-29 16:44:13 -0700

    task102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t lis
+ *
+ * @h: pointer to the first node of the list
+ *
+ * Return: the size of the list
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int d;
+	listint_t *tmp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		d = *h - (*h)->next;
+		if (d > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
+	else if (idx = 1)
+	{
+
+	}
+
 	for (curr = *head, i = 0; i < idx - 1 && curr; curr = curr->next, i++)
 		;
 
