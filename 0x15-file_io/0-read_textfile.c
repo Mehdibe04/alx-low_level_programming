04c3e1d75651293f395e8b2c6f9c4d97b092e4fd
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-09-04 14:35:32 -0700

    all tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: a pointer to the name of the file to be read
+ * @letters: the maximum number of bytes to be read from the file
+ *
+ * Return: number of bytes read from the file (Success),0 otherwise
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t r, w, o;
+	char *buff;
+
+	if (!filename)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || r != w)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	close(o);
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This is the 25th readme file
