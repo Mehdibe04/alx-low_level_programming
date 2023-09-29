23b96d6747d48a71fd2199063b1323d3313ec2d8
Mehdibe04 <belaazrielmehdi@gmail.com>
2023-09-28 18:35:41 -0700

    up

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - func that checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little
+*/
+
+int get_endianness(void)
+{
+	unsigned int pes = 1;
+	char *s;
+
+	s = (char *) & pes;
+	return ((int)*s);
+}
